82d568255476a4de423e2d4d91214257e8573f7f
Guilherme LIC <guilhermelc10@gmail.com>
2024-04-09 19:47:51 -0300

    Special move - castling

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Knight extends ChessPiece{
+	
+	
 
 	public Knight(Board board, Color color) {
 		super(board, color);
+		
 	}
   
 	
@@ -22,6 +26,9 @@ public class Knight extends ChessPiece{
     	ChessPiece p = (ChessPiece)getBoard().piece(position);
     	return p ==null || p.getColor() != getColor();
     }
+    
+  
+     
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -78,6 +85,9 @@ public class Knight extends ChessPiece{
 					
 			mat[p.getRow()][p.getColumn()] = true;
 			  }
+		  
+		   
+		    
 		return mat;
 	}
 }
